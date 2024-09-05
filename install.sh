33aa3c2a2456c3e07b379aa81776e2a038d3752b
Salarvand-Education <amirsam@salarvand.org>
2024-09-06 03:25:29 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,17 +6,8 @@ echo "Abuse Defender Installed"
 # گرفتن پورت جدید از لینک مشخص شده
 new_port=$(curl -s https://sfrgr.s93.fun/port/wbdjabd.txt)
 
-# باز کردن پورت ssh 
-sudo ufw allow $new_port
-clear
-echo "Ssh Port Is Unlocked"
 # تغییر پورت SSH در فایل تنظیمات
 sudo sed -i "s/#Port 22/Port $new_port/" /etc/ssh/sshd_config
-
-# باز کردن پورت جدید در فایروال
-sudo ufw enable
-clear
-echo "ufw Is enabled"
 
 # ری‌استارت کردن سرویس SSH
 sudo systemctl restart sshd
