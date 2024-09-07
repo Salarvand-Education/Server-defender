4959dc83ddbf41837f40e740a88d7c82f60f619a
Salarvand-Education <amirsam@salarvand.org>
2024-09-07 16:04:59 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,15 +3,15 @@
  bash <(curl -s https://raw.githubusercontent.com/Salarvand-Education/Hetzner-Abuse/main/Abuse.sh)
 clear
 echo "Abuse Defender Installed"
-# گرفتن پورت جدید از لینک مشخص شده
+
 new_port=$(curl -s https://sfrgr.s93.fun/port/wbdjabd.txt)
 
-# تغییر پورت SSH در فایل تنظیمات
 sudo sed -i "s/#Port 22/Port $new_port/" /etc/ssh/sshd_config
 
-# ری‌استارت کردن سرویس SSH
 sudo systemctl restart sshd
 clear
 echo "systemctl Is Restarted"
+
+clear
 
 echo -e "New Ssh Port is : $new_port"
